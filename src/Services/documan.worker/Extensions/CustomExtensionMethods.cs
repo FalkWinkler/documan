6d85b02e437b5b5df1f100408097782b9322e2ab
Falk Winkler <fwinkler0573@gmail.com>
2021-06-11 08:49:35 +0200

    work hat jetzt health check

---
+++ src/Services/documan.worker/Extensions/CustomExtensionMethods.cs
@@ -0,0 +1,59 @@
+using Autofac;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+
+namespace documan.worker.Extensions
+{
+    public static class CustomExtensionMethods
+    {
+        public static IServiceCollection AddCustomHealthCheck(this IServiceCollection services, IConfiguration configuration)
+        {
+            var hcBuilder = services.AddHealthChecks();
+
+            hcBuilder.AddCheck("self", () => HealthCheckResult.Healthy());
+
+            //hcBuilder.AddSqlServer(
+            //        configuration["ConnectionString"],
+            //        name: "OrderingTaskDB-check",
+            //        tags: new string[] { "orderingtaskdb" });
+
+            //if (configuration.GetValue<bool>("AzureServiceBusEnabled")) {
+            //    hcBuilder.AddAzureServiceBusTopic(
+            //            configuration["EventBusConnection"],
+            //            topicName: "eshop_event_bus",
+            //            name: "orderingtask-servicebus-check",
+            //            tags: new string[] { "servicebus" });
+            //}
+            //else {
+            //    hcBuilder.AddRabbitMQ(
+            //            $"amqp://{configuration["EventBusConnection"]}",
+            //            name: "orderingtask-rabbitmqbus-check",
+            //            tags: new string[] { "rabbitmqbus" });
+            //}
+
+            return services;
+        }
+
+        public static ILoggingBuilder UseSerilog(this ILoggingBuilder builder, IConfiguration configuration)
+        {
+            var seqServerUrl = configuration["Serilog:SeqServerUrl"];
+            var logstashUrl = configuration["Serilog:LogstashgUrl"];
+
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .Enrich.WithProperty("ApplicationContext", Program.AppName)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.Seq(string.IsNullOrWhiteSpace(seqServerUrl) ? "http://seq" : seqServerUrl)
+                .WriteTo.Http(string.IsNullOrWhiteSpace(logstashUrl) ? "http://logstash:8080" : logstashUrl)
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+
+            return builder;
+        }
+    }
+}
